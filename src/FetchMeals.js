a19e4477be216869d5883a15f0b97bbe51f37490
Julia Naumova <140359113+trueliia@users.noreply.github.com>
2024-04-29 00:19:30 -0500

    Add files via upload

---
+++ src/FetchMeals.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const getAllMeals = (setMeal) => {
+    axios.get("https://meal-plan-wssf.onrender.com")
+    .then(({data}) =>{console.log(data)
+        setMeal(data);
+    })
+}
+
+const addMeal = (title, setTitle, setMeal) =>{
+    axios.post(`https://meal-plan-wssf.onrender.com/saveMeals`, { title })
+    .then((data) =>{
+        console.log(data);
+        setTitle("");
+        getAllMeals(setMeal)
+    })
+}
+
+const editMeal = (mealId, title, setTitle, setMeal, setEditing) =>{
+    axios.put(`https://meal-plan-wssf.onrender.com/editMeal`, { _id: mealId, title })
+    .then((data) =>{
+        console.log(data);
+        setTitle("");
+        setEditing(false);
+        getAllMeals(setMeal)
+    })
+}
+
+const deleteMeal = (_id, setMeal) =>{
+    axios.post(`https://meal-plan-wssf.onrender.com/deleteMeal`, { _id})
+    .then((data) =>{
+        console.log(data);
+        getAllMeals(setMeal)
+    })
+}
+
+export { getAllMeals, addMeal, editMeal, deleteMeal };
---
+++ src/MyMeals.js
@@ -0,0 +1,10 @@
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+
+export const MyMeals = ({ text, updatingInInput, deleteMeal}) =>{
+    return (<div>
+        <p> { text }</p>
+        <AiFillEdit onClick={updatingInInput}></AiFillEdit>
+        <AiFillDelete onClick={deleteMeal}></AiFillDelete>
+    </div>
+    )
+}
